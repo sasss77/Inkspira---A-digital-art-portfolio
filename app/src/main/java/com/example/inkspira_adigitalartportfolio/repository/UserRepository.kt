a172330ab529cfa8654697af0da57b2104703134
sasss77 <saugatpaneru40@gmail.com>
2025-06-03 09:07:12 +0545

    modifications

--- app/src/main/java/com/example/inkspira_adigitalartportfolio/repository/UserRepository.kt
+++ app/src/main/java/com/example/inkspira_adigitalartportfolio/repository/UserRepository.kt
@@ -4,7 +4,7 @@ import com.example.inkspira_adigitalartportfolio.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 
 interface UserRepository {
-    fun login(email : String, password : String, callBack : (Boolean, String) -> Unit)
+    fun login(email : String, password : String, callback : (Boolean, String) -> Unit)
     fun register(email : String, password : String, callback : (Boolean, String, String) -> Unit)
     fun forgetPassword(email : String, callback : (Boolean, String) ->Unit)
     fun getCurrentUser() : FirebaseUser?
@@ -12,4 +12,5 @@ interface UserRepository {
     fun logout(callback : (Boolean, String) ->Unit)
     fun getUserByID(userID : String, callback : (UserModel?, Boolean, String) ->Unit)
     fun updateProfile(userID : String, userData: MutableMap<String, Any?>, callback : (Boolean, String) ->Unit)
+//    fun removeData(userID : String, callback: (Boolean, String) -> Unit)
 }
--- app/src/main/java/com/example/inkspira_adigitalartportfolio/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/inkspira_adigitalartportfolio/repository/UserRepositoryImpl.kt
@@ -3,7 +3,11 @@ package com.example.inkspira_adigitalartportfolio.repository
 import com.example.inkspira_adigitalartportfolio.model.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class UserRepositoryImpl : UserRepository {
 
@@ -11,14 +15,23 @@ class UserRepositoryImpl : UserRepository {
 
     val auth : FirebaseAuth = FirebaseAuth.getInstance()
     val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val ref : DatabaseReference = database.reference.child("users")
 
 
     override fun login(
         email: String,
         password: String,
-        callBack: (Boolean, String) -> Unit
+        callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                callback(true, "Login Successfully")
+            }
+            else {
+                callback(false, "${it.exception?.message}")
+            }
+        }
+
     }
 
     override fun register(
@@ -26,37 +39,85 @@ class UserRepositoryImpl : UserRepository {
         password: String,
         callback: (Boolean, String, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                callback(true, "Registered Successfully", "${auth.currentUser?.uid}")
+            }
+            else {
+                callback(false, "${it.exception?.message}", "")
+            }
+        }
+
     }
 
     override fun forgetPassword(
         email: String,
         callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+
+
+
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+            if(it.isSuccessful) {
+                callback(true, "Reset email sent to $email")
+            }
+            else {
+                callback(false, "${it.exception?.message}")
+            }
+        }
+
+
     }
 
     override fun getCurrentUser(): FirebaseUser? {
-        TODO("Not yet implemented")
+       return auth.currentUser
     }
 
+
+    //Create
     override fun addUserToDatabase(
         userID: String,
         model: UserModel,
         callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+       ref.child(userID).setValue(model).addOnCompleteListener {
+           if(it.isSuccessful) {
+               callback(true, "User added")
+           }
+           else {
+               callback(false, "${it.exception?.message}")
+           }
+       }
     }
 
     override fun logout(callback: (Boolean, String) -> Unit) {
-        TODO("Not yet implemented")
+        try {
+            auth.signOut()
+            callback(true, "Logout successful")
+        } catch ( e : Exception  ) {
+            callback(false, "${e.message}")
+        }
     }
 
     override fun getUserByID(
         userID: String,
         callback: (UserModel?, Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        ref.child(userID).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    val users = snapshot.getValue(UserModel::class.java)
+                    if (users != null ) {
+                        callback(users,true,"data fetched")
+                    }
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(null,false,error.message)
+            }
+        })
     }
 
     override fun updateProfile(
@@ -64,6 +125,16 @@ class UserRepositoryImpl : UserRepository {
         userData: MutableMap<String, Any?>,
         callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        ref.child(userID).updateChildren(userData).addOnCompleteListener {
+            if(it.isSuccessful) {
+                callback(true, "User Updated")
+            }
+            else {
+                callback(false, "${it.exception?.message}")
+            }
+        }
     }
+
+
+
 }
