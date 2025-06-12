8f154b3ca8f7d23e0519e372a91f7a56f46d36a3
sasss77 <saugatpaneru40@gmail.com>
2025-06-12 09:27:20 +0545

    dlt optn working completed

--- app/src/main/java/com/example/inkspira_adigitalartportfolio/view/NavigationActivity.kt
+++ app/src/main/java/com/example/inkspira_adigitalartportfolio/view/NavigationActivity.kt
@@ -3,6 +3,7 @@ package com.example.inkspira_adigitalartportfolio.view
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -230,7 +231,16 @@ fun Home1() {
                                 Icon(Icons.Default.Edit,contentDescription = null)
                             }
 
-                            IconButton(onClick = {},colors = IconButtonDefaults.iconButtonColors(
+                            IconButton(onClick = {
+                                viewModel.deleteProduct(eachProduct?.productID.toString()) {
+                                    success, message ->
+                                    if (success) {
+                                        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                                    }  else {
+                                        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+                                    }
+                                }
+                            },colors = IconButtonDefaults.iconButtonColors(
                                 contentColor = Color.Red
                             )) {
                                 Icon(Icons.Default.Delete,contentDescription = null)
