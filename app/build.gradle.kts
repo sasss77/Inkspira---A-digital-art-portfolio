4c03e32f04ebdd73d2b84f1eaae6592bfe0785bb
sasss77 <saugatpaneru40@gmail.com>
2025-06-13 09:50:16 +0545

    ImageUtils done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.cloudinary:cloudinary-android:2.1.0")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("io.coil-kt:coil-compose:2.2.2")
 }
